d921c15c48173f16147a45837fe1cdd97c838f09
Jeko Tchelidze <jekotchelidze@gmail.com>
2022-07-06 18:56:08 +0400

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,17 @@ $('.main-banner').slick({
   autoplaySpeed:4000
 });
 
+$('.offers-slider').slick({
+  nextArrow: false,
+  prevArrow: false,
+  // infinite: true,
+  prevArrow:false,
+  nextArrow:false,
+  dots: false,
+  autoplay:true,
+  autoplaySpeed:4000
+});
+
 $(".name-box span").click(function(){
     $(".person-box").toggleClass("height-active");
   });
@@ -23,4 +34,17 @@ $(".name-box span").click(function(){
             $(this).text(Math.ceil(now));
         }
     });
+  });
+
+  $('.h-circle').click(function(){
+    $(this).toggleClass('block');
+  });
+  $('.right-t').click(function(){
+    $(this).toggleClass('block');
+  });
+  $('.click-heart').click(function(){
+    $(this).toggleClass('block');
+  });
+  $('.slider-click-h').click(function(){
+    $(this).toggleClass('block');
   });
